332f23b45546e9ecf9c0e848d67694ee74a2b6fa
letengzz <2020885569@qq.com>
2023-02-06 14:27:36 +0800

    MyBatis中<分页操作>代码:添加注释

--- MyBatis/mybatis_paging/src/test/java/com/hjc/test/PagingTest.java
+++ MyBatis/mybatis_paging/src/test/java/com/hjc/test/PagingTest.java
@@ -15,6 +15,22 @@ import java.io.InputStream;
 import java.util.List;
 
 public class PagingTest {
+    /**
+     * MyBatis的分页插件:
+     *  1.添加依赖
+     *  2.在核心配置文件中配置分页插件
+     *  3.在查询功能之前开启分页功能就可以了
+     * <p></p>
+     * pageNum当前页的页码 pageSize每页显示的条数  total数据条数
+     * startRow从第几行开始 endRow从第几行结束 pages一共多少页
+     * navigate导航分页的页码数(一般都是奇数)
+     * list就是获取的page
+     * pageNum当前页码 pagesize每页显示的条数 size当前页的真实程度：当前页显示的真实数量
+     * startRow从几行开始 endRow从几行结束 total总条数 pages总页数
+     * prepage上一页 nextPage下一页  isFristPage是否是第一页 isLastPage是否是最后一页
+     * hasPreviousPage是否有上一页 hasNextPage是否有下一页
+     * navigatepageNums导航分页的页码
+     */
     @Test
     public void testPaging() throws IOException {
         InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
